18bae420455bb21d1b9df7277ace4edeada5532f
Maximus06 <o.maj@free.fr>
2020-06-30 20:36:02 +0200

    add first template

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! in debug mode'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('index.html', name=name)
